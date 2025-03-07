cdc999f850accb8d42eddc7dad2776fd9ed5ac4e
jonatas.araujo <johnlima540@gmail.com>
2025-03-07 14:34:30 -0500

    simplificando a diretiva elementRef

--- api-angular/src/app/shared/fundo-amarelo.directive.ts
+++ api-angular/src/app/shared/fundo-amarelo.directive.ts
@@ -1,17 +1,26 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, inject, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appFundoAmarelo]'
 })
-export class FundoAmareloDirective {
+export class FundoAmareloDirective implements OnInit {
 
-  constructor(private _elementRef: ElementRef , private _renderer: Renderer2) { 
-    // this._elementRef.nativeElement.style.backgroundColor = "yellow"
-    this._renderer.setStyle(
-      this._elementRef.nativeElement,
-      'background-color',
-      'red'
-    );
+  element  = inject(ElementRef).nativeElement as HTMLElement;
+  // constructor(private _elementRef: ElementRef , private _renderer: Renderer2) { 
+
+    
+
+  //    this._elementRef.nativeElement.style.backgroundColor = "yellow"
+  //   // this._renderer.setStyle(
+  //   //   this._elementRef.nativeElement,
+  //   //   'background-color',
+  //   //   'red'
+  //   // );
+  // }
+  ngOnInit(): void {
+    this.element.style.backgroundColor = 'yellow'
+    this.element.style.width = '20%'
+    this.element.style.textAlign = 'center'
   }
 
 }
